4cc98dcc4def22ab6cc7ffb4a6f10b18e8e1b1ba
JhiNResH <mynameisjerry870203@gmail.com>
2024-02-20 17:55:12 -0600

    3

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,9 +1,13 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
 import { createMint } from '@solana/spl-token';
 import wallet from "../wba-wallet.json"
+import bs58 from 'bs58';
+
 
 // Import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const secretKeyString = wallet.secretkey[0];
+const secretKeyArrayBuffer = Buffer.from(bs58.decode(secretKeyString));
+const keypair = Keypair.fromSecretKey(secretKeyArrayBuffer);
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
@@ -13,7 +17,15 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
     try {
         // Start here
         // const mint = ???
-    } catch(error) {
+        const mint = await createMint(
+            connection,
+            keypair,
+            keypair.publicKey,
+            null, // not providing a freeze authority
+            6, // 6 decimal places
+        );
+        console.log(`Mint: ${mint.toBase58()}`);
+    } catch (error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
 })()
